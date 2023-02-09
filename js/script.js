74a46303d8db685c154a313ef6e333739affc1dc
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-09 19:48:14 +0000

    Learning Mode

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  console.log(event.target.textContent);
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+  console.log(mode);
+  if (mode === "learning") {
+    if (document.getElementById(data).textContent === event.target.dataset.answer) {
+      $("#" + data).css("background-color", "green");
+    }
+    else {
+      $("#" + data).css("background-color", "red");
+    }
+  }
+}
+
+function fillQuestions() {
+  let multiplier = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let answers = new Array(10).fill(0);
+  console.log(multiplier.sort(() => Math.random() - 0.5));//Do not comment out
+  for (let i = 0; i < 10; i++) {
+    $("#m" + (i + 1)).text(multiplier[i] + " =");
+    $("#dropBox" + (i + 1)).attr("data-answer", multiplier[i] * 7);
+  }
+  for (let i = 0; i < 10; i++) {
+    answers[i] = multiplier[i];
+  }
+  //console.log(multiplier);
+  console.log(answers.sort((a, b) => a - b));//Do not comment out
+  for (let i = 0; i < 10; i++) {
+    //answersSubset.slice(0,-1).sort();
+    $("#a" + (i + 1)).text(answers[i] * 7);
+  }
+}
+
+$(".switch").on("click", function(event){
+  if($("#learning-mode").is(':checked')){
+    mode = "learning";
+  } else{
+    mode = "testing";
+  }
+})
+
+let mode="testing";
+fillQuestions();
