a8a06a92180f7e47835abc74cdcc482b3e8f43d2
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-10 09:36:50 +0000

    Toggle between modes

--- js/script.js
+++ js/script.js
@@ -41,21 +41,28 @@ function fillQuestions() {
   }
 }
 
-$(".switch").on("click", function(event){
-  if($("#learning-mode").is(':checked')){
+function changeDisplayMode() {
+  if ($("#learning-mode").is(':checked')) {
     mode = "learning";
-    $("#testing-mode-text").css("display","none");
-    $("#learning-mode-text").css("display","inline");
-    $("#submit").css("display","none");
-  } else{
+    localStorage.setItem("mode", mode);
+    $("#testing-mode-text").css("display", "none");
+    $("#learning-mode-text").css("display", "inline");
+    $("#submit").css("display", "none");
+  } else {
     mode = "testing";
-    $("#testing-mode-text").css("display","inline");
-    $("#learning-mode-text").css("display","none");
-    $("#submit").css("display","inline");
+    localStorage.setItem("mode", mode);
+    $("#testing-mode-text").css("display", "inline");
+    $("#learning-mode-text").css("display", "none");
+    $("#submit").css("display", "inline");
   }
+}
+
+$(".switch").on("click", function (event) {
+  changeDisplayMode();
+  location.reload();
 })
 
-$("#submit").click(function(){
+$("#submit").click(function () {
   console.log("Checking");
   let scoreCorrect = 0;
   let scoreWrong = 0;
@@ -63,18 +70,18 @@ $("#submit").click(function(){
   for (let i = 0; i < 10; i++) {
     //$("#m" + (i + 1)).text(multiplier[i] + " =");
     console.log($("#dropBox" + (i + 1)).attr("data-answer"));
-    if ($("#dropBox" + (i + 1)).children('p').length == 1){
-      if($("#dropBox" + (i + 1)).children('p')[0].textContent === $("#dropBox" + (i + 1)).attr("data-answer")){
+    if ($("#dropBox" + (i + 1)).children('p').length == 1) {
+      if ($("#dropBox" + (i + 1)).children('p')[0].textContent === $("#dropBox" + (i + 1)).attr("data-answer")) {
         scoreCorrect++;
-        $("#dropBox" + (i + 1)).css("background-color","green");
-      } else{
+        $("#dropBox" + (i + 1)).css("background-color", "green");
+      } else {
         scoreWrong++;
-        $("#dropBox" + (i + 1)).css("background-color","red");
+        $("#dropBox" + (i + 1)).css("background-color", "red");
       }
-    } else{
+    } else {
       console.log("undefined");
       scoreNoAttempt++;
-      $("#dropBox" + (i + 1)).css("background-color","yellow");
+      $("#dropBox" + (i + 1)).css("background-color", "yellow");
     }
   }
   console.log(scoreCorrect);
@@ -84,5 +91,17 @@ $("#submit").click(function(){
   $("#exampleModalCenter").modal();
 })
 
-let mode="testing";
+let mode = localStorage.getItem("mode");
+if (mode === null) {
+  mode = "testing";
+  //localStorage.setItem("mode", mode);
+  $("#learning-mode").prop("checked", false);
+} else if (mode === "testing") {
+  $("#learning-mode").prop("checked", false);
+} else {
+  mode = "learning";
+  //localStorage.setItem("mode", mode);
+  $("#learning-mode").prop("checked", true);
+}
+changeDisplayMode();
 fillQuestions();
