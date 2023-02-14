3600148ca05786c9541f64c291afdab1c89e3235
Prabhav Nadipi Reddy <prabhav.nr@gmail.com>
2023-02-14 09:17:30 +0000

    Barebones game done

--- animation-game/js/animation-game.js
+++ animation-game/js/animation-game.js
@@ -1,36 +1,36 @@
+var myBot = document.getElementById("myBot");   
+var input = document.getElementById("fname");
+var myMonster = document.getElementById("myAnimation");   
 
 var id = null;
 var posMonster = -150;
 function myMove() {
-  var elem = document.getElementById("myAnimation");   
   clearInterval(id);
-  id = setInterval(frame, 10);
+  id = setInterval(frame, 100);
   function frame() {
     if (posMonster == window.innerWidth-100) {
-      elem.src=elem.dataset.still;
+      myMonster.src=myMonster.dataset.still;
       clearInterval(id);
     } else {
       posMonster++; 
-      elem.style.left = posMonster + 'px'; 
+      myMonster.style.left = posMonster + 'px'; 
       if(posMonster>=posBot){
         clearInterval(id);
         console.log("You Lost!");
-        elem.src="https://media1.giphy.com/media/1mgObw4w1GZR0vmnRe/200_s.gif";
+        myMonster.src="https://media1.giphy.com/media/1mgObw4w1GZR0vmnRe/200_s.gif";
       }
     }
   }
 }
 
 var posBot = 00;
-function moveBot() {
-  var elem = document.getElementById("myBot");   
-  if (posBot >= window.innerWidth-100) {
-    
-  } else {
+$("#submit").on("click", function moveBot(event) {
+  event.preventDefault();
+  if (posBot < window.innerWidth-100 && input.value === "14") {
     posBot+=50; 
     //elem.style.top = pos + 'px'; 
-    elem.style.left = posBot + 'px'; 
-    console.log(elem.src);
-    elem.src=elem.dataset.animate;
+    myBot.style.left = posBot + 'px'; 
+    console.log(myBot.src);
+    myBot.src="https://media1.giphy.com/media/l3q2wuKKE8qQ9YvMQ/200.gif";
   }
-}
+});
