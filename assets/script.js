6bbd56a2f079ad9fd6b269290ffcf3720864c8d4
may <mtruong@hotmail.co.uk>
2023-02-15 19:32:58 +0000

    fixed home buttons, added backgrounds, linked pages

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,1 @@
-function startQuiz(){
-    let startScreenElement = document.getElementById("start-screen");
-    startScreenElement.setAttribute("class", "hide");
 
-    questionsElement.removeAttribute("class");
-
-    timerID = setInterval(clockTick, 1000)
-
-    timerElement.textContent = time;
-
-    getQuestion();
-
-}
